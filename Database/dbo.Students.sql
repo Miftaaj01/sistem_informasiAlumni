f45de6027924c451010d25fa1363dca39ae422de
Miftaaj <75676233+Miftaaj01@users.noreply.github.com>
2021-04-29 00:08:25 +0700

    Add files via upload

---
+++ Database/dbo.Students.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Students] (
+    [Std_Id]    INT           IDENTITY (1, 1) NOT NULL,
+    [firstName] NVARCHAR (50) NOT NULL,
+    [lastName]  NVARCHAR (50) NOT NULL,
+    [contact]   NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Std_Id] ASC)
+);
+
